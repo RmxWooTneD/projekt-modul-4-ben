809c61f2cf2c0a416a03eba07d957a9c8efbc90d
Tom Volland <rmxwootned@gmail.com>
2025-08-03 19:13:49 +0200

    finish Quiz App

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const questions = [
       {
         answer: "Hanover",
         anserId: "a",
-        correct: true,
+        correct: false,
       },
       {
         answer: "Berlin",
         anserId: "b",
-        correct: false,
+        correct: true,
       },
       {
         answer: "Hamburg",
@@ -85,20 +85,33 @@ const questions = [
 let newQuestion = questions[0];
 console.log(newQuestion);
 // ____________________________
-
-const quizHtmlEl = document.querySelector(".display-question");
 let newQuestionIndex = "";
 let questionIndex = 0;
-let allQuestions = "";
+const quizHtmlEl = document.querySelector(".display-question");
 const quizTitle = document.querySelector(".question-title");
 const quizAnswer1 = document.querySelector(".answer1");
 const quizAnswer2 = document.querySelector(".answer2");
 const quizAnswer3 = document.querySelector(".answer3");
 const quizAnswer4 = document.querySelector(".answer4");
+const AnswerEl = document.querySelector(".question-answers");
 
 function nextQestion() {
   renderQuiz();
-  loadNextQuestion();
+  if (questionIndex === 0) {
+    alert("Viel Spaß beim Quiz!");
+  } else if (questionIndex === 1) {
+    alert("Hier ist die zweite Frage!");
+  } else if (questionIndex === 2) {
+    alert("Hier ist die letzte Frage!");
+  }
+  // Überprüfe, ob die letzte Frage erreicht wurde
+  else if (questionIndex === 3) {
+    alert(
+      "Das war die letzte Frage! Du hast das Quiz erfolgreich abgeschlossen!"
+    );
+    alert("Klicke auf 'Weiter' um das Quiz zu beenden.");
+    quizHtmlEl.innerHTML = `<div class="end-message">Quiz beendet! Vielen Dank fürs Mitmachen!</div>`;
+  }
 }
 
 function renderQuiz() {
@@ -108,48 +121,88 @@ function renderQuiz() {
   console.log(questionTitle);
 
   let answer1 = newQuestion.answers[0].answer;
-  console.log(answer1);
   let answer2 = newQuestion.answers[1].answer;
   let answer3 = newQuestion.answers[2].answer;
   let answer4 = newQuestion.answers[3].answer;
 
-  let html = newQuestion;
+  let html = "";
 
   // setze HTML und Werte
   html += `<div class="question">
         <div class="question-title">${questionTitle}</div>
         <div class="question-answers">
-          <button class="answer answer1 incorrect" onclick="">${answer1}</button>
-          <button class="answer answer2 correct" onclick="">${answer2}</button>
-          <button class="answer answer3" onclick="">${answer3}</button>
-          <button class="answer answer4" onclick="">${answer4}</button>
+          <button id="a" class="answer answer1" onclick= check()>${answer1}</button>
+          <button id="b" class="answer answer2" onclick= check()>${answer2}</button>
+          <button id="c" class="answer answer3" onclick= check()>${answer3}</button>
+          <button id="d" class="answer answer4" onclick= check()>${answer4}</button>
         </div>
-      </div>`;
+      </div>
+          <div>
+      <button class="footer" onclick="showAnswer()">Lösung</button>
+      <button class="footer" onclick="nextQestion()">Weiter</button>
+    </div>`;
 
   quizHtmlEl.innerHTML = html;
 }
 
-function loadNextQuestion() {
-  questions.forEach((element) => {
-    return (allQuestions = questions);
+function showAnswer() {
+  quizHtmlEl.querySelectorAll(".answer").forEach((el) => {
+    if (questionIndex === 0) {
+      if (el.id === "b") {
+        el.classList.add("correct");
+      } else {
+        el.classList.add("incorrect");
+      }
+    } else if (questionIndex === 1) {
+      if (el.id === "c") {
+        el.classList.add("correct");
+      } else {
+        el.classList.add("incorrect");
+      }
+    } else if (questionIndex === 2) {
+      if (el.id === "d") {
+        el.classList.add("correct");
+      } else {
+        el.classList.add("incorrect");
+      }
+    }
   });
+  loadNextQuestion();
+}
 
-  console.log(allQuestions);
-
+function loadNextQuestion() {
   if (questionIndex === 0) {
-    newQuestionIndex = ++questionIndex;
-    console.log(newQuestionIndex);
+    questionIndex = ++questionIndex;
+    console.log(questionIndex);
     newQuestion = questions[1];
-    console.log(newQuestion);
-  } else {
-    newQuestionIndex = ++questionIndex;
-    console.log(newQuestionIndex);
+  } else if (questionIndex === 1) {
+    questionIndex = ++questionIndex;
+    console.log(questionIndex);
     newQuestion = questions[2];
-    console.log(newQuestion);
+  } else if (questionIndex === 2) {
+    questionIndex = ++questionIndex;
+    console.log(questionIndex);
   }
 }
 
-// _________________________________________________________
+function check() {
+  if (questionIndex === 0) {
+    document.getElementById("b").classList.add("correct");
+    document.getElementById("a").classList.add("incorrect");
+    document.getElementById("c").classList.add("incorrect");
+    document.getElementById("d").classList.add("incorrect");
+  } else if (questionIndex === 1) {
+    document.getElementById("a").classList.add("incorrect");
+    document.getElementById("b").classList.add("incorrect");
+    document.getElementById("c").classList.add("correct");
+    document.getElementById("d").classList.add("incorrect");
+  } else if (questionIndex === 2) {
+    document.getElementById("a").classList.add("incorrect");
+    document.getElementById("b").classList.add("incorrect");
+    document.getElementById("c").classList.add("incorrect");
+    document.getElementById("d").classList.add("correct");
+  }
+  loadNextQuestion();
+}
 
-// Lösung anzeigen
-// Weiter Button neue frage rendern
+// _________________________________________________________
